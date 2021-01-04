ddcee014330eee9e88568a4d576b3390d80e0f21
Imaad Uddin <imaaduddin@icloud.com>
2021-01-03 23:35:39 -0600

    worked on the light and dark mode feature

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 const toggleSwitch = document.querySelector("input[type='checkbox']");
+const nav = document.getElementById("nav");
+const toggleIcon = document.getElementById("toggle-icon");
+const image1 = document.getElementById("image1");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+const textBox = document.getElementById("text-box");
+
+// dark mode styles 
+function darkMode() {
+  nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.remove("fa-sun");
+  toggleIcon.children[1].classList.add("fa-moon");
+  image1.src = "Images/undraw_proud_coder_dark.svg";
+  image2.src = "Images/undraw_feeling_proud_dark.svg";
+  image3.src = "Images/undraw_conceptual_idea_dark.svg";  
+}
+
 
 // switch theme dynamically 
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
+    darkMode();
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    lightMode();
   }
 }
 
