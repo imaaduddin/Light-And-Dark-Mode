54755fb80e4d790d524158ef91346c2ab346c13a
Imaad Uddin <imaaduddin@icloud.com>
2021-01-10 21:37:49 -0600

    try to get the same functionality with fewer lines of code

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ function darkMode() {
   nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
   textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
   toggleIcon.children[0].textContent = "Dark Mode";
-  toggleIcon.children[1].classList.remove("fa-sun");
-  toggleIcon.children[1].classList.add("fa-moon");
+  toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
   image1.src = "Images/undraw_proud_coder_dark.svg";
   image2.src = "Images/undraw_feeling_proud_dark.svg";
   image3.src = "Images/undraw_conceptual_idea_dark.svg";  
@@ -23,8 +22,7 @@ function lightMode() {
   nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
   textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
   toggleIcon.children[0].textContent = "Light Mode";
-  toggleIcon.children[1].classList.remove("fa-moon");
-  toggleIcon.children[1].classList.add("fa-sun");
+  toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
   image1.src = "Images/undraw_proud_coder_light.svg";
   image2.src = "Images/undraw_feeling_proud_light.svg";
   image3.src = "Images/undraw_conceptual_idea_light.svg";  
