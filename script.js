74a3f271483d6e7566d35910f8bca00798749611
Imaad Uddin <imaaduddin@icloud.com>
2021-01-17 23:38:57 -0600

    finalized local storage

--- script.js
+++ script.js
@@ -51,4 +51,9 @@ toggleSwitch.addEventListener("change", switchTheme);
 const currentTheme = localStorage.getItem("theme");
 if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
 }
